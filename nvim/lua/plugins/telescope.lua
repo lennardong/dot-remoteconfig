16f062a8d204d8b69492e60c8d8cbcdbc6468dde
LO <lennard@lumigaia.com>
2026-04-02 06:39:31 +0800

    feat(telescope): scope find/grep to git repo and add project switcher
    Multi-repo workspace causes find_files and live_grep to search across
    all repos. Switch ff to git_files, scope fw to git root, and add fp
    to cd between sibling repos via telescope picker.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -35,8 +35,45 @@ return {
     telescope.load_extension("fzf")
   end,
   keys = {
-    { "<leader>ff", "<cmd>Telescope find_files<cr>",             desc = "Find Files" },
-    { "<leader>fw", "<cmd>Telescope live_grep<cr>",              desc = "Live Grep" },
+    {
+      "<leader>ff",
+      function()
+        require("telescope.builtin").git_files({ show_untracked = true })
+      end,
+      desc = "Find Files (repo)",
+    },
+    {
+      "<leader>fw",
+      function()
+        local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+        if vim.v.shell_error == 0 then
+          require("telescope.builtin").live_grep({ cwd = git_root })
+        else
+          require("telescope.builtin").live_grep()
+        end
+      end,
+      desc = "Live Grep (repo)",
+    },
+    {
+      "<leader>fp",
+      function()
+        local parent = vim.fn.fnamemodify(vim.fn.getcwd(), ":h")
+        require("telescope.builtin").find_files({
+          cwd = parent,
+          find_command = { "fd", "--type", "d", "--max-depth", "1" },
+          attach_mappings = function(_, map)
+            map("i", "<CR>", function(prompt_bufnr)
+              local entry = require("telescope.actions.state").get_selected_entry(prompt_bufnr)
+              require("telescope.actions").close(prompt_bufnr)
+              vim.cmd.cd(parent .. "/" .. entry[1])
+              vim.notify("cd → " .. entry[1])
+            end)
+            return true
+          end,
+        })
+      end,
+      desc = "Switch Project",
+    },
     { "<leader>fb", "<cmd>Telescope buffers<cr>",                desc = "Buffers" },
     { "<leader>cp", "<cmd>Telescope commands<cr>",               desc = "Command Palette" },
     { "gr",          "<cmd>Telescope lsp_references<cr>",         desc = "References" },
