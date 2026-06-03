4b802708a186b2861c2977ef4bce6382a48391e3
LO <lennard@lumigaia.com>
2026-06-03 14:43:02 +0800

    feat(tmux): sort order by letter

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -11,6 +11,7 @@ return {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = "make",
     },
+    "nvim-telescope/telescope-file-browser.nvim",
   },
   config = function()
     local telescope = require("telescope")
@@ -40,6 +41,7 @@ return {
       },
     })
     telescope.load_extension("fzf")
+    telescope.load_extension("file_browser")
   end,
   keys = {
     {
@@ -85,6 +87,64 @@ return {
       desc = "Switch Project",
     },
     { "<leader>fb", "<cmd>Telescope buffers<cr>",                desc = "Buffers" },
+    { "<leader>fe", "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>", desc = "File Browser" },
+    {
+      "<leader>fc",
+      function()
+        require("telescope.builtin").git_commits({
+          attach_mappings = function(prompt_bufnr, map)
+            local actions = require("telescope.actions")
+            local action_state = require("telescope.actions.state")
+            local show_commit = function()
+              local entry = action_state.get_selected_entry()
+              actions.close(prompt_bufnr)
+              local sha = entry.value
+              vim.cmd("tabnew")
+              vim.cmd("setlocal buftype=nofile bufhidden=wipe filetype=git nobuflisted")
+              vim.api.nvim_buf_set_lines(0, 0, -1, false,
+                vim.fn.systemlist({ "git", "show", "--stat", "-p", sha }))
+              vim.cmd("0")
+            end
+            actions.select_default:replace(show_commit)
+            map("i", "<C-o>", show_commit)
+            return true
+          end,
+        })
+      end,
+      desc = "Git Commits (view)",
+    },
+    {
+      "<leader>fC",
+      function()
+        require("telescope.builtin").git_bcommits({
+          attach_mappings = function(prompt_bufnr, _)
+            local actions = require("telescope.actions")
+            local action_state = require("telescope.actions.state")
+            actions.select_default:replace(function()
+              local entry = action_state.get_selected_entry()
+              actions.close(prompt_bufnr)
+              local sha = entry.value
+              vim.cmd("tabnew")
+              vim.cmd("setlocal buftype=nofile bufhidden=wipe filetype=git nobuflisted")
+              vim.api.nvim_buf_set_lines(0, 0, -1, false,
+                vim.fn.systemlist({ "git", "show", "--stat", "-p", sha }))
+              vim.cmd("0")
+            end)
+            return true
+          end,
+        })
+      end,
+      desc = "Git Buffer Commits (view)",
+    },
+    {
+      "<leader>fs",
+      function()
+        local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+        local opts = vim.v.shell_error == 0 and { cwd = git_root } or {}
+        require("telescope.builtin").grep_string(opts)
+      end,
+      desc = "Grep word under cursor",
+    },
     { "<leader>cp", "<cmd>Telescope commands<cr>",               desc = "Command Palette" },
     { "gr",          "<cmd>Telescope lsp_references<cr>",         desc = "References" },
     { "<leader>ds",  "<cmd>Telescope lsp_document_symbols<cr>",   desc = "Document Symbols" },
